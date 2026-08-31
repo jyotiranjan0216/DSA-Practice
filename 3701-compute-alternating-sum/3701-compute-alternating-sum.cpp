2e34410a24596dbc0e0a9055609addc0e5dca24f
Jyotiranjan Mishra <164573774+jyotiranjan0216@users.noreply.github.com>
2026-08-31 22:44:21 +0530

    Time: 0 ms (100%), Space: 32.5 MB (79.66%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int sum1 = 0, sum2 = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(i & 1) sum2 += nums[i];
+            else sum1 += nums[i];
+        }
+        return sum1 - sum2;
+    }
+};
